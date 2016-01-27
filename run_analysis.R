5fac226be431adae6df66f74d5494098215cfebe
Shirley-Xian <yoyohua@hotmail.com>
2016-01-27 19:51:10 +0800

    Corrected the result

--- run_analysis.R
+++ run_analysis.R
@@ -1,13 +1,13 @@
 #Step 1
 #Get variable names of features
 features <- read.table("UCI HAR Dataset/features.txt", sep = "", header = FALSE)
+features <- trimws(gsub("\\W+"," ", features$V2))
 
 #Get train subject list
 subject <-  read.table("UCI HAR Dataset/train/subject_train.txt", sep = "", header = FALSE, col.names = "subject")
 
-#Step 4
 #Get train data 
-x <- read.table("UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE, col.names = features$V2)
+x <- read.table("UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE, col.names = features)
 #Get train activity data
 activity <-  read.table("UCI HAR Dataset/train/Y_train.txt", sep = "", header = FALSE, col.names = "activity")
 #Build train data set
@@ -15,9 +15,9 @@ train <- cbind(subject, x,activity, category = "train")
 
 #Get test subject list
 subject <-  read.table("UCI HAR Dataset/test/subject_test.txt", sep = "", header = FALSE, col.names = "subject")
-#Step 4
+
 #Get test data
-x <- read.table("UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE, col.names = features$V2)
+x <- read.table("UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE, col.names = features)
 #Get test activity list
 activity <-  read.table("UCI HAR Dataset/test/Y_test.txt", sep = "", header = FALSE, col.names = "activity")
 #Build test data set
@@ -27,7 +27,7 @@ dat <- rbind(train,test)
 
 #Step 2
 #Get names of columns we need 
-names <- grep("\\.mean\\.|\\.std\\.", names(dat),value = TRUE)
+names <- grep("\\.mean\\.|\\.mean$|\\.std\\.|\\.std$", names(dat),value = TRUE)
 
 #Get collumns we need
 part <- dat[,c("subject","activity","category", names)]
@@ -38,10 +38,14 @@ activities <- read.table("UCI HAR Dataset/activity_labels.txt", sep = "", header
 
 #Add activity label to dataset
 labeled <- merge(part,activities, by.x = "activity", by.y = "activity")[,-1]
-
+      
 #Step 5
 library(dplyr)
 grouped <- group_by(labeled, activityLabel,subject) 
 result <- summarize_each(grouped,funs(mean),3:68)
+
+#Step 4
+names(result) <- gsub("\\W+"," ", names(result))
+
 result
 #write.table(result, file = "ProjectResult.txt",row.name = FALSE)
